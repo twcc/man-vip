a2b96570a2da904af8d65bf314204f96cf9aff97
augustchao <august.chao@twsc.io>
2022-02-23 18:35:46 +0800

    change edit to git.twcc.ai

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,8 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://git.twcc.ai/twcc/manual/-/edit/189-docusaurus/',
         },
         blog: {
           showReadingTime: true,
