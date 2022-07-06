70f67a8bbbdb27e584c2c89004a041c89fdfb835
VIGA-WEI\Viga Wei <viga.wei@twsc.io>
2022-07-06 10:24:21 +0800

    update documents

--- docs/services.md
+++
@@ -1,1 +0,0 @@
-# TWCC Service list
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import clsx from 'clsx';
-import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -17,12 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Crash Tutorial - 5min ⏱️
-          </Link> | 
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            HPC Tutorial
+            Quick Guide - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -34,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
